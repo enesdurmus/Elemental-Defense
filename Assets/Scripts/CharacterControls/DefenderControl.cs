66a54a48cb177c320763f20d3f616d2b25463b76
Enes <enesdurmus3738@gmail.com>
2021-07-10 20:54:03 +0300

    Handle Fire particle effect

--- Assets/Scripts/CharacterControls/DefenderControl.cs
+++ Assets/Scripts/CharacterControls/DefenderControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DefenderControl : MonoBehaviour
 {
-
+    [SerializeField] private GameObject flameThrowSkill;
     [SerializeField] private Material placebleMat;
     [SerializeField] private Material notPlacebleMat;
     private SkinnedMeshRenderer clothes;
@@ -12,6 +12,15 @@ public class DefenderControl : MonoBehaviour
     private void Start()
     {
         clothes = transform.Find("RiggedCharacter").transform.Find("Cult Leader").GetComponent<SkinnedMeshRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Instantiate(flameThrowSkill, transform.Find("FlameThrowPoint").transform);
+            Debug.Log("alo");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
